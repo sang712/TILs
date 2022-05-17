df21545d8c2c99f000a86b6375a291665c2800b2
sang712 <tkdrlf95@gmail.com>
2022-05-17 16:17:25 +0900

    Add the second answer of PGRM_완주하지 못한 선수 [Level 1 - 해시] 완주하지 못한 선수

---
+++ Algorithm/Programmers/PGRM_완주하지못한선수_2.py
@@ -0,0 +1,19 @@
+# 출처: 프로그래머스 - 다른 사람의 풀이
+# 해시를 사용한 풀이
+
+def solution(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        # 해시 값으로 dictionary에 저장
+        dic[hash(part)] = part
+        # 해시 값들을 모두 더함
+        temp += hash(part) # hash는 숫자 값
+    for com in completion:
+        # 해시 값들을 뺌
+        temp -= hash(com)
+    # 남은 해시값의 사람을 dictionary에서 뽑아서 출력
+    answer = dic[temp]
+
+    return answer
