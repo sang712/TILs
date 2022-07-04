92493dab9d13f2d6b50525ce3aab0156e2f71552
Sanggil Han <tkdrlf95@naver.com>
2022-07-04 20:40:55 +0900

    Add another answer of BOJ1932 [실버 1] 정수 삼각형 다이나믹 프로그래밍

--- Algorithm/BOJ1932_정수 삼각형.py
+++ Algorithm/BOJ1932_정수 삼각형.py
@@ -14,3 +14,37 @@ for i in range(N):
         else:
             triangle[j] = triangle[j-1] + line[j]
 print(max(triangle))
+
+'''
+<다른 풀이>
+
+왼쪽에 0을 추가한 줄과 오른쪽에 0을 추가한 줄, input 값을 zip()으로 묶음
+왼쪽에 0을 추가한 줄과 오른쪽에 0을 추가한 줄은 트리의 좌, 우 값을 갖게 됨
+해당 값을 비교함으로써 max 값을 선택할 수 있고 zip()으로 묶은 값만을 더하면 끝
+'''
+'''
+import sys
+n = int(input())
+dp_n = []
+
+for _ in range(n):
+    dp_b = list(map(int, sys.stdin.readline().split()))
+    dp_cache = []
+    for n_t1, n_t2, n_b1 in zip([0]+dp_n, dp_n+[0], dp_b):
+        print("print: ", n_t1, n_t2, n_b1)
+        dp_cache.append(max(n_t1, n_t2) + n_b1)
+    dp_n = dp_cache
+print(max(dp_n))
+'''
+'''
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+cache = []
+
+for i in range(n):
+    floor = list(map(int, read().split()))
+    cache = [max(a+c, b+c) for a, b, c in zip([0]+cache, cache+[0], floor)]
+print(max(cache))
+'''
