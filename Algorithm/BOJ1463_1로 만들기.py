6ae3dfd30923c880ad23b48fd90022dade729a98
Sanggil Han <tkdrlf95@naver.com>
2022-07-07 23:13:05 +0900

    Add an answer of BOJ1463 [실버 3] 1로 만들기 다이나믹 프로그래밍

--- Algorithm/BOJ1463_1로 만들기.py
+++ Algorithm/BOJ1463_1로 만들기.py
@@ -1,24 +1,23 @@
-def calculate(X):
-    a, b, c = -1, -1, -1
-    if X % 3 == 0:
-        a = X // 3
-    if X % 2 == 0:
-        b = X // 2
-    c = X - 1
-    return [a, b, c]
+'''
+이렇게 해당 수에 2나 3을 곱해서 그 수를 찾는 것 보다
+수를 하나하나 올라가며 나눠지는지 확인하고
+그 전 수에 해당하는 것에 1을 더해 비교하는 것이 300ms 더 빠름
+지금은 924ms 나옴
+'''
 
 N = int(input())
 
-dp = [set(int(input()))]
-
-for nums in range(dp):
-    temp = set()
-    for num in nums:
-        a, b, c = calculate(num)
-        if a > 0: temp.add(a)
-        if b > 0: temp.add(b)
-        if c > 0: temp.add(c)
-        if a == 1 or b == 1 or c == 1:
-            pass
-    dp.append(temp)
-    
+dp = [-1 for _ in range(N+1)]
+dp[1] = 0
+for num in range(1, N+1):
+    times = dp[num] + 1
+    X1 = num * 3
+    if X1 <= N:
+        dp[X1] = times if dp[X1] == -1 else min(times, dp[X1])
+    X2 = num * 2
+    if X2 <= N:
+        dp[X2] = times if dp[X2] == -1 else min(times, dp[X2])
+    X3 = num + 1
+    if X3 <= N:
+        dp[X3] = times if dp[X3] == -1 else min(times, dp[X3])
+print(dp[N])
