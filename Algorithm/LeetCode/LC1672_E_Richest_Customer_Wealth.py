cf3bdf8cc132d2b882399bcee2004262f41e90a1
Sanggil Han <tkdrlf95@naver.com>
2022-10-12 22:24:48 +0900

    Add an answer of LC1672 [Easy] Richest Customer Wealth 배열, 행렬

---
+++ Algorithm/LeetCode/LC1672_E_Richest_Customer_Wealth.py
@@ -0,0 +1,10 @@
+'''
+요구사항대로 구현하였음
+'''
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth = 0
+        for customer in accounts:
+            wealth = sum(customer)
+            if wealth > max_wealth: max_wealth = wealth
+        return max_wealth
