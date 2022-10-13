3421787af416735364c1b28d5d8ac1a31309b5aa
Sanggil Han <tkdrlf95@naver.com>
2022-10-13 23:10:02 +0900

    Add an answer of LC237 [Medium] Delete Node in a Linked List 연결 리스트

---
+++ Algorithm/LeetCode/LC237_M_Delete_Node_in_a_Linked_List.py
@@ -0,0 +1,21 @@
+'''
+연결 리스트에서 중간 노드를 자르고 앞과 뒤 노드를 연결하는 함수를 완성하는 문제
+문제에서 head를 주지 않고, input과 주어진 함수의 입력은 서로 달라서 문제를 이해하기 어려웠음
+return을 하지 말라는 것에 영감을 얻어서 구현만 하면 된다고 생각했고
+다음과 같이 구현하였음
+그렇지만 node = node.next가 왜 안 되는 것인지 이해는 되지 않음
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
