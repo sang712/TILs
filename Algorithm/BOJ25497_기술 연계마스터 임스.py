84d7a44d7c910f9d4e2bd1048ec4f8f37325cf31
Sanggil Han <tkdrlf95@naver.com>
2023-08-05 00:08:47 +0900

    Add an answer of BOJ25497
    [실버 4] 기술 연계마스터 임스
    
    구현, 자료 구조, 스택

--- Algorithm/BOJ25497_기술 연계마스터 임스.py
+++ Algorithm/BOJ25497_기술 연계마스터 임스.py
@@ -1,27 +1,29 @@
+"""
+포인트는 중간에 스킬 연계에 실패하면 그 이후의 스킬 모두가 발동되지 않는다는 점이며
+직전에 사용한 연계 사전 스킬이 어떤 것이였느냐와는 무관하다는 점이다
+"""
 N = int(input())
 skills = input()
 
-ans = 0
-skill_without_num = []
+ans = Ss = Ls = 0
 for skill in skills:
     if '1' <= skill <= '9':
         ans += 1
-    else:
-        skill_without_num.append(skill)
-
-Ss = 0
-Ls = 0
-for skill in skill_without_num:
-    if skill == 'S':
+    elif skill == 'S':
         Ss += 1
     elif skill == 'L':
         Ls += 1
-    elif skill == 'K' and Ss:
-        Ss -= 1
-        ans += 1
-    elif skill == 'R' and Ls:
-        Ls -= 1
-        ans += 1
+    elif skill == 'K':
+        if Ss:
+            Ss -= 1
+            ans += 1
+        else:
+            break
+    elif skill == 'R':
+        if Ls:
+            Ls -= 1
+            ans += 1
+        else:
+            break
     
 print(ans)
-    
