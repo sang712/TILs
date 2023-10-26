ef3f4de10e30df5c00fbf31634ac9534598d5231
Sanggil Han <tkdrlf95@naver.com>
2023-10-27 00:01:29 +0900

    Add an answer of BOJ1075

--- Algorithm/BOJ1075_나누기.py
+++ Algorithm/BOJ1075_나누기.py
@@ -2,7 +2,7 @@ N = int(input())
 F = int(input())
 
 for i in range(100):
-    num = (N % 100) * 100 + i
+    num = (N // 100) * 100 + i
     if num % F == 0:
         print(i if i >= 10 else '0' + str(i))
         break
