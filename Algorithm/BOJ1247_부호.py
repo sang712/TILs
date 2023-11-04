6cb60a5b4fc6d890d119b2720bfcce032a78bddd
Sanggil Han <tkdrlf95@naver.com>
2023-11-04 23:54:30 +0900

    Add an answer of BOJ1247
    [브론즈 3] 부호
    
    수학, 사칙연산, 임의 정밀도 / 큰 수 연산

---
+++ Algorithm/BOJ1247_부호.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(3):
+    N = int(input())
+    ans = 0
+    for _ in range(N):
+        ans += int(input())
+    if ans == 0:
+        print(0)
+    elif ans > 0:
+        print('+')
+    else:
+        print('-')
