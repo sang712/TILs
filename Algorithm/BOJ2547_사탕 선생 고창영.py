9f2720a0fa090ee8841e2bb3b213a17dfbba2ac7
Sanggil Han <tkdrlf95@naver.com>
2023-11-16 00:07:03 +0900

    Add an answer of BOJ2547
    [브론즈 3] 사탕 선생 고창영
    
    수학, 사칙연산, 임의 정밀도 / 큰 수 연산

--- Algorithm/BOJ2547_사탕 선생 고창영.py
+++ Algorithm/BOJ2547_사탕 선생 고창영.py
@@ -1,21 +1,16 @@
-T = int(input())
-_ = input()
-num = 0
-cnt = 0
-for _ in range(T):
-    try: 
-        candy = input()
-        while not candy == '':
-            num += int(candy)
-            cnt += 1
-            candy = input()
-        
-        if num % cnt == 0:
-            print('YES')
-        else:
-            print('NO')
-        num, cnt = 0, 0
-    except EOFError:
-        break
+import sys
+input = sys.stdin.readline 
 
+T = int(input())
+num = 0
+for _ in range(T):
+    _ = input()
+    N = int(input())
+    for _ in range(N):
+        num += int(input())
         
+    if num % N == 0 or num == 0:
+        print('YES')
+    else:
+        print('NO')
+    num = 0
