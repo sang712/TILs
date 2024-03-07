cdaa0c1b961c31c1cfb58a5135116c2a389ac3e2
Sanggil Han <tkdrlf95@naver.com>
2024-03-07 15:27:04 +0900

    Add an answer of BOJ6986
    [실버 3] 절사평균
    
    구현, 정렬

--- Algorithm/BOJ6986_절사평균.py
+++ Algorithm/BOJ6986_절사평균.py
@@ -1,19 +1,14 @@
+"""
+문제의 데이터가 의도하길 float형(혹은 double형)을 사용하면 오차가 나게 되어있어서
+오차의 값을 없애기 위해 0.0000001 10e-7을 더해주었음
+10e-6부터는 틀린답을 냄
+"""
 import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-scores = []
-for _ in range(N):
-    a = float(input())
-    scores.append(a)
+
+scores = [float(input()) for _ in range(N)]
 scores.sort()
-if K == 0:
-    print('{:.2f}'.format(sum(scores)/len(scores)))
-    print('{:.2f}'.format(sum(scores) / len(scores)))
-else:
-    cut = scores[K:-K]
-    corrected = scores.copy()
-    for i in range(K):
-        corrected[i], corrected[-i-1] = corrected[K], corrected[-K-1]
-    print('{:.2f}'.format(sum(cut)/len(cut)+ 0.00000001))
-    print('{:.2f}'.format(sum(corrected)/len(corrected)+ 0.00000001))
+print(f'{sum(scores[K:N-K])/(N-2*K) + 0.0000001:.2f}')
+print(f'{(K*scores[K] + sum(scores[K:N-K]) + scores[N-K-1]*K)/N + 0.0000001:.2f}')
