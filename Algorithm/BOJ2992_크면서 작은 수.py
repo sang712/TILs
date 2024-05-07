52a044081b014a34abe25ac0613d2bd7f36fe8a9
Sanggil Han <tkdrlf95@naver.com>
2024-05-07 23:05:19 +0900

    Add an answer of BOJ2992
    [실버 3] 크면서 작은 수
    
    브루트포스 알고리즘, 백트래킹

---
+++ Algorithm/BOJ2992_크면서 작은 수.py
@@ -0,0 +1,30 @@
+"""
+뒤에서부터 숫자를 하나씩 떼어 스택 자료 구조에 넣고
+기존 숫자의 마지막 수와 스택의 마지막 숫자를 비교해서 기존 숫자의 마지막 수가 작으면
+스택의 그 마지막 숫자를 넣고 스택을 정렬한 뒤
+해당 숫자보다 큰 수 중 가장 작은 수를 찾아 기존 숫자의 마지막에 배치함
+그 후에 기존 수와 정렬된 스택의 수를 합쳐 반환하는 방식으로 풀이하였음
+"""
+import sys
+input = sys.stdin.readline
+
+num = list(input().strip())
+stack = []
+for i in range(len(num)):
+    stack.append(num.pop())
+    if num:
+        if num[-1] >= stack[-1]:
+            continue
+        else:
+            lastnum = num[-1]
+            stack.append(num.pop())
+            stack.sort()
+            for i in range(len(stack)):
+                if lastnum < stack[i]:
+                    num.append(stack.pop(i))
+                    break
+            num += stack
+            print(''.join(num))
+            break
+else:
+    print(0)
