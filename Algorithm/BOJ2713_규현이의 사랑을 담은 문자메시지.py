354c932d1a42b8bf3f0667228437093c2a3ec2aa
Sanggil Han <tkdrlf95@naver.com>
2024-05-12 23:56:56 +0900

    Add an answer of BOJ2713
    [실버 2] 규현이의 사랑을 담은 문자메시지

---
+++ Algorithm/BOJ2713_규현이의 사랑을 담은 문자메시지.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+code = {' ': '00000', 'A': '00001', 'B': '00010', 'C': '00011', 'D': '00100',
+        'E': '00101', 'F': '00110', 'G': '00111', 'H': '01000', 'I': '01001',
+        'J': '01010', 'K': '01011', 'L': '01100', 'M': '01101', 'N': '01110',
+        'O': '01111', 'P': '10000', 'Q': '10001', 'R': '10010', 'S': '10011',
+        'T': '10100', 'U': '10101', 'V': '10110', 'W': '10111', 'X': '11000',
+        'Y': '11001', 'Z': '11010'}
+delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+for t in range(int(input())):
+    raw = input()
+    R = int(raw[:2])
+    raw = raw[2:].lstrip()
+    C = int(raw[:2])
+    raw = raw[2:]
+    message = raw[1:-2] if C >= 10 else raw[:-2]
+    board = [['0']*C for _ in range(R)]
+    visited = [[0]*C for _ in range(R)]
+    message = ''.join([code[word] for word in ' '.join(message)])
+    r, c = 0, 0
+    i = 0
+    for char in message:
+        if not visited[r][c]:
+            visited[r][c] = 1
+            board[r][c] = char
+        else:
+            i = (i + 1) % 4
+            dr, dc = delta[i]
+            r, c = r + dr, c + dc
+            board[r][c] = char
+            visited[r][c] = 1
+        dr, dc = delta[i]
+        nr, nc = r + dr, c + dc
+        for _ in range(4):
+            if not (0 <= nr < R and 0 <= nc < C and not visited[nr][nc]):
+                i = (i + 1) % 4
+                dr, dc = delta[i]
+                nr, nc = r + dr, c + dc
+            else:
+                break
+        r, c = nr, nc
+    print(''.join(map(''.join, board)))
